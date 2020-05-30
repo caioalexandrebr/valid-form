d5edda41dea75fd700b2be5cbc2304e49547fd9e
Caio Alexandre <contato@caioalexandre.com>
2020-05-30 16:46:34 -0300

    feat(validate): add validation script

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,47 @@
-$(function() {
-  console.log( "ready!" );
+$(function () {
+  var validate = (function () {
+    function form(selector) {
+      let input = $(selector).find("input");
+      let check = true;
+
+      input.map(function (index, value) {
+        if ($(value).hasClass("email")) {
+          if (email($(value).val())) {
+            boolean(input.eq(index), true);
+          } else {
+            check = false;
+            boolean(input.eq(index), check);
+          }
+        }
+      });
+
+      return check;
+    };
+
+    function boolean(input, value) {
+      value ? input.closest(".form-group").removeClass("error") : input.closest(".form-group").addClass("error");
+    };
+
+    function email(string) {
+      let reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+      return (reg.test(string) ? true : false);
+    };
+
+    function required(string) {
+      return (string.length > 0 ? true : false);
+    };
+
+    return {
+      form,
+      email,
+      required,
+    };
+  })();
+
+  $("#form").on("submit", function (e) {
+    e.preventDefault();
+    if (validate.form($(this))) {
+      alert("Success!");
+    }
+  });
 });
